a3ffb89def3eb8d0007fd2f893704a0d9e27c881
Thr0wW <androstr06@gmail.com>
2021-11-08 00:55:43 +0400

    Update OtmazkiOtDerevny.py

--- OtmazkiOtDerevny.py
+++ OtmazkiOtDerevny.py
@@ -1,39 +1,43 @@
 import random
 
-Otmazki = ['деревенский климат', 'много ковров', 'скучно', 'нет компьютера', 'старый дом', 'нет покоя', 'аллергия на плесень', 'аллергия на солнце', 'аллергия на траву','аллергия на деревню']
 
+Otmazki = ['деревенский климат', 'много ковров', 'скучно', 'нет компьютера', 'старый дом', 'нет покоя', 'аллергия на плесень', 'аллергия на солнце', 'аллергия на траву','аллергия на деревню']
+Otveti = ['В этом же нет ничего плохого!','Пропылесось', 'Найдем чем тебе занятся', 'Зато отдохнешь от него!', 'Не такой он уж и старый', 'И ЧО?', 'Не такая она уж и большая', 'С зонтиком ходи', 'Пруфы!', 'Нет']
 print("\033[31m" + '\033[1m' + 'Почему ты не хочешь ехать в деревню?')
 input("\033[37m" + "\033[2m" + "\033[3m" + "Enter для продолжения")
 print('\033[0m')
+response = '\033[4m\033[1m\033[32mВ деревне '
+a=('\n\033[0m\033[2m\033[3m')
 while True:
-    otmazka = random.choice(Otmazki)
-    if otmazka == 'деревенский  климат':
-        input('\033[4m\033[1m\033[32mВ деревне ' +otmazka + '\n\033[0m\033[2m\033[3mВ этом же нет ничего плохого!')
+    otmazka1 = random.choice(Otmazki)
+    otvet= random.choice(Otveti)
+    if otmazka1 == 'деревенский  климат':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('деревенский  климат')
-    elif otmazka == 'много ковров':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mПропылесось')
+    elif otmazka1 == 'много ковров':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('много ковров')
-    elif otmazka == 'скучно':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mНайдем чем тебе занятся')
+    elif otmazka1 == 'скучно':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('скучно')
-    elif otmazka == 'нет компьютера':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mЗато отдохнешь от него!')
+    elif otmazka1 == 'нет компьютера':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('нет компьютера')
-    elif otmazka == 'старый дом':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mНе такой он уж и старый')
+    elif otmazka1 == 'старый дом':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('старый дом')
-    elif otmazka == 'нет покоя':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mИ ЧО?')
+    elif otmazka1 == 'нет покоя':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('нет покоя')
-    elif otmazka == 'аллергия на плесень':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mНе такая она уж и большая')
+    elif otmazka1 == 'аллергия на плесень':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('аллергия на плесень')
-    elif otmazka == 'аллергия на солнце':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mС зонтиком ходи')
+    elif otmazka1 == 'аллергия на солнце':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('аллергия на солнце')
-    elif otmazka == 'аллергия на траву':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mПруфы!')
+    elif otmazka1 == 'аллергия на траву':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('аллергия на траву')
-    elif otmazka == 'аллергия на деревню':
-        input('\033[4m\033[1m\033[32mВ деревне ' + otmazka + '\n\033[0m\033[2m\033[3mНет')
+    elif otmazka1 == 'аллергия на деревню':
+        input(response + otmazka1 + '\n' + otvet)
         Otmazki.remove('аллергия на деревню')
